dfb4e89879958b63c4d05b1c691f387882ecbfa0
Pavel Alekseev <alekseevpo@gmail.com>
2026-02-13 09:26:28 +0100

    Исправлено OG изображение для Telegram
    - Заменен SVG файл на PNG для совместимости с Telegram
    - Использовано существующее изображение our_team.png
    - Обновлены Open Graph метаданные
    - Теперь картинки отображаются во всех мессенджерах

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -88,7 +88,7 @@ export async function generateMetadata({
             siteName: 'Selen.IT Digital Agency',
             images: [
                 {
-                    url: '/og-image.png',
+                    url: '/our_team.png',
                     width: 1200,
                     height: 630,
                     alt: 'Selen.IT Digital Agency - Web Development & Design',
@@ -99,7 +99,7 @@ export async function generateMetadata({
             card: 'summary_large_image',
             title: dict.hero.title1 + ' ' + dict.hero.titleGradient + ' ' + dict.hero.title2,
             description: dict.hero.subtitle,
-            images: ['/og-image.png'],
+            images: ['/our_team.png'],
             creator: '@selen_it',
             site: '@selen_it',
         },
